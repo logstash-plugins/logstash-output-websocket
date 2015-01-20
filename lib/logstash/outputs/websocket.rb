0abe980c4df8b3448650d10a7058cd55e2a25bd4
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-01-20 15:37:11 -0500

    Removing milestone option

--- lib/logstash/outputs/websocket.rb
+++ lib/logstash/outputs/websocket.rb
@@ -11,7 +11,6 @@ require "logstash/outputs/base"
 # If no clients are connected, any messages received are ignored.
 class LogStash::Outputs::WebSocket < LogStash::Outputs::Base
   config_name "websocket"
-  milestone 1
 
   # The address to serve websocket data from
   config :host, :validate => :string, :default => "0.0.0.0"
