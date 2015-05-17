ca7536cfcf321cef374066f80505702329a691b1
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-05-17 20:19:44 +0200

    Version 0.1.5 bump, including very small registry test plus some missing dependencies.

--- spec/outputs/websocket_spec.rb
+++ spec/outputs/websocket_spec.rb
@@ -1,1 +1,12 @@
+# encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
+require "logstash/outputs/websocket"
+
+describe "output/websocket" do
+
+  subject(:output) { LogStash::Outputs::WebSocket.new }
+
+  it "should register" do
+    expect {output.register}.to_not raise_error
+  end
+end
